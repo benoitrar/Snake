78d49c0805c7f3d196b7ad61ee90b18a32de24f1
Benedek Kiss <benedek_kiss@epam.com>
2014-01-09 10:06:05 +0100

    refactored Menu class' structure and removed controller functions

--- src/Menu.java
+++ src/Menu.java
@@ -1,6 +1,3 @@
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
@@ -22,80 +19,38 @@ public class Menu extends JMenuBar {
     private final JMenuItem navigation = new JMenuItem("Irányítás");
 
     public Menu() {
-        // Az Új Játék, a Toplista és a Kilépés funkciók hozzárendelése
-        newGame.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                // TODO
-                //reset();
-            }
-        });
-        toplist.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                // TODO
-                //JOptionPane.showMessageDialog(jatekter, scrollpane);
-            }
-        });
-        exit.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                System.exit(0);
-            }
-        });
+        composeGameMenuPoint();
+        composeSettingsMenuPoint();
+        composeHelpMenuPoint();
+        
+        addMenuPointsToMainMenu();
+    }
 
-        // Ezek hozzáadása a Játék menüponthoz
+    private void composeGameMenuPoint() {
         game.add(newGame);
         game.addSeparator();
         game.add(toplist);
         game.addSeparator();
         game.add(exit);
-        this.add(game);
+    }
 
-        // A sebesség változtatásának hozzárendelése
-        difficult.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                // TODO
-                //sebesseg = 50;
-            }
-        });
-        normal.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                // TODO
-                //sebesseg = 70;
-            }
-        });
-        easy.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                // TODO
-                //sebesseg = 90;
-            }
-        });
-
-        // Ezek hozzáadása a Beállítások menüponthoz
+    private void composeSettingsMenuPoint() {
         settings.add(difficult);
         settings.addSeparator();
         settings.add(normal);
         settings.addSeparator();
         settings.add(easy);
-        this.add(settings);
+    }
 
-        // A segítségek funkcióinak megvalósítása
-        creator.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                // TODO
-                //JOptionPane.showMessageDialog(jatekter, "Készítõ: Kérlek Refaktorálj\n" + "Programnév: Snake\n" + "Verziószám: v0.7");
-            }
-        });
-        navigation.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                // TODO
-                //JOptionPane.showMessageDialog(jatekter, "Irányítás a kurzor segítségével:\n" + "-Fel nyíl: a kígyó felfele mozog\n"
-                  //      + "-Le nyíl: a kígyó lefele mozog\n" + "-Jobbra nyíl: a kígyó jobbra mozog\n" + "-Balra nyíl: a kígyó balra mozog\n");
-            }
-        });
-
-        // Ezek hozzáadása a Segítség menüponthoz
+    private void composeHelpMenuPoint() {
         help.add(creator);
         help.addSeparator();
         help.add(navigation);
+    }
+
+    private void addMenuPointsToMainMenu() {
+        this.add(game);
+        this.add(settings);
         this.add(help);
     }
 
