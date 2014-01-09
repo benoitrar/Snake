5052ee23eda5ea19d4442a1c2be79b12fbbd9db9
Benedek Kiss <benedek_kiss@epam.com>
2014-01-09 09:59:38 +0100

    changed fields' name to english

--- src/Menu.java
+++ src/Menu.java
@@ -7,19 +7,19 @@ import javax.swing.JMenuItem;
 
 public class Menu extends JMenuBar {
 
-    private final JMenu jatek = new JMenu("Játék");
+    private final JMenu game = new JMenu("Játék");
     private final JMenuItem newGame = new JMenuItem("Új Játék (F2)");
     private final JMenuItem toplist = new JMenuItem("Toplista");
-    private final JMenuItem kilepes = new JMenuItem("Kilépés (ALT+F4)");
+    private final JMenuItem exit = new JMenuItem("Kilépés (ALT+F4)");
     
-    private final JMenu beallitasok = new JMenu("Beállítások");
-    private final JMenuItem nehez = new JMenuItem("Nehéz");
+    private final JMenu settings = new JMenu("Beállítások");
+    private final JMenuItem difficult = new JMenuItem("Nehéz");
     private final JMenuItem normal = new JMenuItem("Normál");
-    private final JMenuItem konnyu = new JMenuItem("Könnyû");
+    private final JMenuItem easy = new JMenuItem("Könnyû");
     
-    private final JMenu segitseg = new JMenu("Segítség");
-    private final JMenuItem keszito = new JMenuItem("Készítõ");
-    private final JMenuItem iranyitas = new JMenuItem("Irányítás");
+    private final JMenu help = new JMenu("Segítség");
+    private final JMenuItem creator = new JMenuItem("Készítõ");
+    private final JMenuItem navigation = new JMenuItem("Irányítás");
 
     public Menu() {
         // Az Új Játék, a Toplista és a Kilépés funkciók hozzárendelése
@@ -35,22 +35,22 @@ public class Menu extends JMenuBar {
                 //JOptionPane.showMessageDialog(jatekter, scrollpane);
             }
         });
-        kilepes.addActionListener(new ActionListener() {
+        exit.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
                 System.exit(0);
             }
         });
 
         // Ezek hozzáadása a Játék menüponthoz
-        jatek.add(newGame);
-        jatek.addSeparator();
-        jatek.add(toplist);
-        jatek.addSeparator();
-        jatek.add(kilepes);
-        this.add(jatek);
+        game.add(newGame);
+        game.addSeparator();
+        game.add(toplist);
+        game.addSeparator();
+        game.add(exit);
+        this.add(game);
 
         // A sebesség változtatásának hozzárendelése
-        nehez.addActionListener(new ActionListener() {
+        difficult.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
                 // TODO
                 //sebesseg = 50;
@@ -62,7 +62,7 @@ public class Menu extends JMenuBar {
                 //sebesseg = 70;
             }
         });
-        konnyu.addActionListener(new ActionListener() {
+        easy.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
                 // TODO
                 //sebesseg = 90;
@@ -70,21 +70,21 @@ public class Menu extends JMenuBar {
         });
 
         // Ezek hozzáadása a Beállítások menüponthoz
-        beallitasok.add(nehez);
-        beallitasok.addSeparator();
-        beallitasok.add(normal);
-        beallitasok.addSeparator();
-        beallitasok.add(konnyu);
-        this.add(beallitasok);
+        settings.add(difficult);
+        settings.addSeparator();
+        settings.add(normal);
+        settings.addSeparator();
+        settings.add(easy);
+        this.add(settings);
 
         // A segítségek funkcióinak megvalósítása
-        keszito.addActionListener(new ActionListener() {
+        creator.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
                 // TODO
                 //JOptionPane.showMessageDialog(jatekter, "Készítõ: Kérlek Refaktorálj\n" + "Programnév: Snake\n" + "Verziószám: v0.7");
             }
         });
-        iranyitas.addActionListener(new ActionListener() {
+        navigation.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
                 // TODO
                 //JOptionPane.showMessageDialog(jatekter, "Irányítás a kurzor segítségével:\n" + "-Fel nyíl: a kígyó felfele mozog\n"
@@ -93,10 +93,10 @@ public class Menu extends JMenuBar {
         });
 
         // Ezek hozzáadása a Segítség menüponthoz
-        segitseg.add(keszito);
-        segitseg.addSeparator();
-        segitseg.add(iranyitas);
-        this.add(segitseg);
+        help.add(creator);
+        help.addSeparator();
+        help.add(navigation);
+        this.add(help);
     }
 
     public JMenuItem getNewGame() {
